6c1bd74086d2e8b13555d85c3f9aded6be5b6719
Rahul Deo <inform.rahuldeo@gmail.com>
2024-02-24 19:36:54 +0530

    Initial implementation

---
+++ src/main/java/com/rahul/security/SpringbootsecurityjwtApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecurityjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsecurityjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/security/SpringbootsecurityjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
